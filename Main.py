196595ea828207d405f660db281a512686245ec8
rfrankeltcsbell <bellmore+rfrankel@thecoderschool.com>
2024-12-11 21:30:11 +0000

    first commit

---
+++ Main.py
@@ -0,0 +1,18 @@
+grocerylist={
+"Lettace":1,
+"Garlic":2,
+"Tomatoe":4,
+"Squash":6,
+"Chicken":1,
+"Meat":1,
+"Cumcumbers":2,
+
+}
+for list in grocerylist:
+    print(list)
+
+tempurature= 57
+input = int(input("enter a number")) 
+if input < tempurature 
+else 
+print("Oy vey it's cold outside")
